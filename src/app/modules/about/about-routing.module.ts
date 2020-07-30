630d6e3fdd127958e18d61f8926eeb100358a178
swapnil <ddevil691@gmail.com>
2020-07-30 14:30:04 +0530

    Basic SAP complete

--- src/app/modules/about/about-routing.module.ts
+++ src/app/modules/about/about-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about.component';
+import { AboutOneComponent } from './about-one/about-one.component';
+import { AboutTwoComponent } from './about-two/about-two.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AboutComponent,
+    children: [
+      {
+        path: 'about-one',
+        component: AboutOneComponent
+      },
+      {
+        path: 'about-two',
+        component: AboutTwoComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { HomeOneComponent } from './home-one/home-one.component';
+import { HomeTwoComponent } from './home-two/home-two.component';
 
-const routes: Routes = [{
-  path: '',
-  component: HomeComponent
-}];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'home-one',
+        component: HomeOneComponent
+      }, {
+        path: 'home-two',
+        component: HomeTwoComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
