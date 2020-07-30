da7e8d399e96b950673c7d6435974a871eb2019d
swapnil <ddevil691@gmail.com>
2020-07-30 15:01:58 +0530

    Added hardcored id in app init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare const ga: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-google-analytics';
+
+  ngOnInit() {
+    console.log(ga);
+    ga('create', 'UA-174052756-1', 'auto');
+    ga('send', 'pageview');
+  }
 }
